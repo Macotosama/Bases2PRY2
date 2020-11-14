20f22930d5a2eeabb5e140946d091e77f43f8dd7
macotosama <jtn1999@outlook.com>
2020-11-14 11:22:31 -0600

    Update

--- Bases2PRY2Interfaz/src/app/newcategory/newcategory.component.ts
+++ Bases2PRY2Interfaz/src/app/newcategory/newcategory.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-newcategory',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./newcategory.component.scss']
 })
 export class NewcategoryComponent implements OnInit {
+
+  nameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(16),
+  ]);
 
   constructor() { }
 
